a7a4135822291f2693e29cf76179fc315a9da790
kimalittlestar <kim_alittlestar@qq.com>
2019-11-05 11:13:00 +0800

    增加了一个add函数的限制和一些注释

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
  * @brief 这是一个函数 ，是所有函数的入口
  * 增加一行函数描写
  * 又增加一行函数
+ * 又增加一行注释
  * @date Create at 2019-11-01
  * @note AllCopyRight by XXX CO ,. LTD
  **********************************************************************
@@ -15,5 +16,6 @@ void main(void)
 
 int add(int a,int b)
 {
-    return a+b;
+    if(a != 0 && b != 0)
+        return a+b;
 }
